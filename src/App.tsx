1efc11a7c9d1c1a7a78832e73c9dbaf5377442f7
MaxAnii <ansarbaba2000@gmail.com>
2024-09-28 20:43:05 +0530

    feat: get the highlighted text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	const [text, setText] = useState("");
+	const getSelectedText = async () => {
+		try {
+			let [tab] = await chrome.tabs.query({
+				active: true,
+				currentWindow: true,
+			});
+			if (!tab?.id) {
+				console.error("No active tab found or invalid tab ID.");
+				return;
+			}
+			await chrome.scripting.executeScript({
+				target: { tabId: tab.id },
+				func: () => {
+					let selectedText =
+						window.getSelection()?.toString().trim() || "No text selected";
+					chrome.runtime.sendMessage({ selectedText });
+				},
+			});
+		} catch (error) {
+			console.error("Failed to execute script:", error);
+		}
+	};
+	useEffect(() => {
+		getSelectedText();
+		const handleMessage = (message: { selectedText: string }) => {
+			if (message.selectedText) {
+				setText(message.selectedText);
+			}
+		};
+		chrome.runtime.onMessage.addListener(handleMessage);
+		return () => {
+			chrome.runtime.onMessage.removeListener(handleMessage);
+		};
+	}, []);
+	return (
+		<div className="App">
+			<p>{text}</p>
+		</div>
+	);
 }
 
 export default App;
